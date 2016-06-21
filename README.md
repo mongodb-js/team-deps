f93db8616cff260d4f322bb1833ca284ffe79b66
Chris Shum <chris.shum@mongodb.com>
2016-06-21 16:43:52 -0400

    Update pull args, new README
    pull args option changed from -d to -s for clarity
    README updated with explanation and examples

--- README.md
+++ README.md
@@ -2,10 +2,57 @@
 
 > Module that gives extended commands to work with npm dependencies in a nodde project
 
+When working on a large node project, it may rely on many node modules, some of which may actually be modules from your organization but separated into modules for maintainability and testability. It may be necessary to edit/work with these modules but current tools for working with team dependencies may be difficult to maintain. This tool uses existing npm and git commands but chains and automates them for ease of use.
+
+## Commands
+
+```javascript
+team-deps -h
+```
+
+```
+Usage: index.js <command> [options]
+
+Commands:
+  list [dir] [options]     List all npm module dependencies of a node project
+  clone [dir] [options]    Clone all npm module dependencies of a node project
+  check [dir] [options]    Check that all npm module dependencies are up to date
+  update [dir] [options]   Update all npm module dependencies against a
+                           package.json
+  pull [source] [options]  Run git pull for all dependencies of a node project
+
+Options:
+  -h, --help  Show help                                                [boolean]
+
+One command required
+```
+
 ## Example
 
 ```javascript
+team-deps list
 ```
+will list all the npm production dependencies of the project in the current directory
+
+```javascript
+team-deps list <dir>
+```
+will list all the npm production dependencies of a project in the directory that you specify
+
+```javascript
+team-deps list -o <organization>
+```
+will list all the npm production dependencies of the project in the current directory but filters for only the dependencies from the specified organization
+
+```javascript
+team-deps clone -d <destination>
+```
+will clone the git repos of the current project's npm dependencies to the specified destination directory
+
+```javascript
+team-deps pull -s <destination>
+```
+will git pull all the repos (only if they are on master branch) of the current project's npm dependencies at the specified source directory
 
 ## License
 
--- index.js
+++ index.js
@@ -1,90 +1,92 @@
-var yargs = require('yargs');
-var npm_functions = require('./lib/index.js')
+/* eslint camelcase:0 */
 
-var options = yargs
+// var yargs = require('yargs');
+var npm_functions = require('./lib/index.js');
+
+var yargs = require('yargs')
   .usage('Usage: $0 <command> [options]')
   .command('list [dir] [options]', 'List all npm module dependencies of a node project',
-    function (yargs) {
+    function() {
       return yargs.option('organization', {
         alias: 'o',
         describe: 'apply a filter to list only npm dependencies from a specified Github organization',
         default: null
-      })
+      });
     },
-    function (argv) {
+    function(argv) {
       npm_functions.list({
         dir: argv.dir,
         o: argv.o
-      })
+      });
     }
   )
   .command('clone [dir] [options]', 'Clone all npm module dependencies of a node project',
-    function (yargs) {
+    function() {
       return yargs.options({
-        'destination' : {
+        'destination': {
           alias: 'd',
           describe: 'a destination directory to clone all git repositories to',
           default: null
         },
-        'organization' : {
+        'organization': {
           alias: 'o',
           describe: 'apply a filter to clone only npm dependencies from a specified Github organization',
           default: null
         }
-      })
+      });
     },
-    function (argv) {
+    function(argv) {
       npm_functions.clone({
         dir: argv.dir,
         d: argv.d,
         o: argv.o
-      })
+      });
     }
   )
   .command('check [dir] [options]', 'Check that all npm module dependencies are up to date',
-    function (yargs) {
-      return; //no options supported yet
+    function() {
+      return; // no options supported yet
     },
-    function (argv) {
+    function(argv) {
       npm_functions.check({
-        dir: argv.dir,
-      })
+        dir: argv.dir
+      });
     }
   )
   .command('update [dir] [options]', 'Update all npm module dependencies against a package.json',
-    function (yargs) {
-      return; //no options supported yet
+    function() {
+      return; // no options supported yet
     },
-    function (argv) {
+    function(argv) {
       npm_functions.update({
-        dir: argv.dir,
-      })
+        dir: argv.dir
+      });
     }
   )
-  .command('pull [dir] [options]', 'Run git pull for all dependencies of a node project',
-    function (yargs) {
+  .command('pull [source] [options]', 'Run git pull for all dependencies of a node project',
+    function() {
       return yargs.options({
-        'destination' : {
-          alias: 'd',
-          describe: 'a destination directory to find all related git repositories ',
+        'source': {
+          alias: 's',
+          describe: 'a source directory to find all related git repositories ',
           default: null
         },
-        'organization' : {
+        'organization': {
           alias: 'o',
           describe: 'apply a filter to pull only npm dependencies from a specified Github organization',
           default: null
         }
-      })
+      });
     },
-    function (argv) {
+    function(argv) {
       npm_functions.pull({
         dir: argv.dir,
-        d: argv.d,
+        d: argv.s,
         o: argv.o
-      })
+      });
     }
   )
-  .demand(1, "One command required")
+  .demand(1, 'One command required')
   .help('h')
   .alias('h', 'help')
-  .argv
+  .argv;
